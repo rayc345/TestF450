28df2526c8ac0cd241309f5b2209724b1a6a06b2
Ray Zhang <eggplant886@gmail.com>
2023-05-29 16:52:59 +0800

    SPI Nor Flash Test

--- main.c
+++ main.c
@@ -36,108 +36,81 @@ OF SUCH DAMAGE.
 #include <stdbool.h>
 #include "gd32f4xx.h"
 #include "systick.h"
+#include "gd25qxx.h"
 
-uint8_t tx_buffer[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
-                       0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
-                       0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F,
-                       0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F,
-                       0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F,
-                       0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x5B, 0x5C, 0x5D, 0x5E, 0x5F,
-                       0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F,
-                       0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A, 0x7B, 0x7C, 0x7D, 0x7E, 0x7F,
-                       0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F,
-                       0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9A, 0x9B, 0x9C, 0x9D, 0x9E, 0x9F,
-                       0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF,
-                       0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xB5, 0xB6, 0xB7, 0xB8, 0xB9, 0xBA, 0xBB, 0xBC, 0xBD, 0xBE, 0xBF,
-                       0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF,
-                       0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6, 0xD7, 0xD8, 0xD9, 0xDA, 0xDB, 0xDC, 0xDD, 0xDE, 0xDF,
-                       0xE0, 0xE1, 0xE2, 0xE3, 0xE4, 0xE5, 0xE6, 0xE7, 0xE8, 0xE9, 0xEA, 0xEB, 0xEC, 0xED, 0xEE, 0xEF,
-                       0xF0, 0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0xF7, 0xF8, 0xF9, 0xFA, 0xFB, 0xFC, 0xFD, 0xFE, 0xFF};
+#define BUFFER_SIZE 256
+#define TX_BUFFER_SIZE (countof(tx_buffer) - 1)
+#define RX_BUFFER_SIZE 0xFF
 
-#define ARRAYNUM(arr_name) (uint32_t)(sizeof(arr_name) / sizeof(*(arr_name)))
-uint8_t rx_buffer[ARRAYNUM(tx_buffer)];
+#define countof(a) (sizeof(a) / sizeof(*(a)))
 
-#define USART2_DATA_ADDRESS ((uint32_t)0x40004804)
+#define FLASH_WRITE_ADDRESS 0x000000
+#define FLASH_READ_ADDRESS FLASH_WRITE_ADDRESS
+
+uint8_t tx_buffer[256];
+uint8_t rx_buffer[256];
+
+ErrStatus memory_compare(uint8_t *src, uint8_t *dst, uint16_t length);
 
 int main(void)
 {
     /* configure systick */
     systick_config();
 
-    rcu_periph_clock_enable(RCU_GPIOC);
+    spi_flash_init();
 
-    /* enable USART clock */
-    rcu_periph_clock_enable(RCU_USART2);
+    uint32_t flash_id = spi_flash_read_id();
+    printf("Flash ID %X\n", flash_id);
 
-    gpio_mode_set(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO_PIN_15);
+    printf("\n\rWrite to tx_buffer:\n");
 
-    /* connect port to USART2_Tx */
-    gpio_af_set(GPIOC, GPIO_AF_7, GPIO_PIN_10);
-
-    /* connect port to USART2_Rx */
-    gpio_af_set(GPIOC, GPIO_AF_7, GPIO_PIN_11);
-
-    /* configure USART Tx as alternate function push-pull */
-    gpio_mode_set(GPIOC, GPIO_MODE_AF, GPIO_PUPD_PULLUP, GPIO_PIN_10);
-    gpio_output_options_set(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_10);
-
-    /* configure USART Rx as alternate function push-pull */
-    gpio_mode_set(GPIOC, GPIO_MODE_AF, GPIO_PUPD_PULLUP, GPIO_PIN_11);
-    gpio_output_options_set(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_11);
-
-    /* USART configure */
-    usart_deinit(USART2);
-    usart_baudrate_set(USART2, 115200U);
-    usart_receive_config(USART2, USART_RECEIVE_ENABLE);
-    usart_transmit_config(USART2, USART_TRANSMIT_ENABLE);
-    usart_enable(USART2);
-
-    dma_single_data_parameter_struct dma_init_struct;
-    /* enable DMA0 */
-    rcu_periph_clock_enable(RCU_DMA0);
-    /* deinitialize DMA0 channel3(USART2 tx) */
-    dma_deinit(DMA0, DMA_CH3);
-    dma_init_struct.direction = DMA_MEMORY_TO_PERIPH;
-    dma_init_struct.memory0_addr = (uint32_t)tx_buffer;
-    dma_init_struct.memory_inc = DMA_MEMORY_INCREASE_ENABLE;
-    dma_init_struct.periph_memory_width = DMA_PERIPH_WIDTH_8BIT;
-    dma_init_struct.number = ARRAYNUM(tx_buffer);
-    uint32_t uPeriAddr = USART2_DATA_ADDRESS;
-    dma_init_struct.periph_addr = uPeriAddr;
-    dma_init_struct.periph_inc = DMA_PERIPH_INCREASE_DISABLE;
-    dma_init_struct.priority = DMA_PRIORITY_ULTRA_HIGH;
-    dma_single_data_mode_init(DMA0, DMA_CH3, &dma_init_struct);
-    /* configure DMA mode */
-    dma_circulation_disable(DMA0, DMA_CH3);
-    dma_channel_subperipheral_select(DMA0, DMA_CH3, DMA_SUBPERI4);
-
-    dma_deinit(DMA0, DMA_CH1);
-    dma_init_struct.direction = DMA_PERIPH_TO_MEMORY;
-    dma_init_struct.memory0_addr = (uint32_t)rx_buffer;
-    dma_single_data_mode_init(DMA0, DMA_CH1, &dma_init_struct);
-    /* configure DMA mode */
-    dma_circulation_disable(DMA0, DMA_CH1);
-    dma_channel_subperipheral_select(DMA0, DMA_CH1, DMA_SUBPERI4);
-
-    /* enable USART2 DMA channel transmission and reception */
-    dma_channel_enable(DMA0, DMA_CH3);
-    dma_channel_enable(DMA0, DMA_CH1);
-
-    /* USART DMA enable for transmission and reception */
-    usart_dma_transmit_config(USART2, USART_TRANSMIT_DMA_ENABLE);
-    usart_dma_receive_config(USART2, USART_RECEIVE_DMA_ENABLE);
-
-    /* wait until USART2 TX DMA0 channel transfer complete */
-    while (RESET == dma_flag_get(DMA0, DMA_CH3, DMA_INTF_FTFIF))
+    /* printf tx_buffer value */
+    for (uint16_t i = 0; i < BUFFER_SIZE; i++)
     {
+        tx_buffer[i] = i;
+        printf("0x%X ", tx_buffer[i]);
+
+        if (15 == i % 16)
+        {
+            printf("\n");
+        }
+        delay_1ms(1);
     }
 
-    /* wait until USART2 RX DMA0 channel transfer complete */
-    while (RESET == dma_flag_get(DMA0, DMA_CH1, DMA_INTF_FTFIF))
+    printf("\nRead from rx_buffer:\n");
+
+    /* erase the specified flash sector */
+    spi_flash_sector_erase(FLASH_WRITE_ADDRESS);
+
+    /* write tx_buffer data to the flash */
+    spi_flash_buffer_write(tx_buffer, FLASH_WRITE_ADDRESS, 256);
+
+    delay_1ms(10);
+    /* read a block of data from the flash to rx_buffer */
+    spi_flash_buffer_read(rx_buffer, FLASH_READ_ADDRESS, 256);
+
+    /* printf rx_buffer value */
+    for (uint16_t i = 0; i < BUFFER_SIZE; i++)
     {
+        printf("0x%X ", rx_buffer[i]);
+        if (15 == i % 16)
+        {
+            printf("\n");
+        }
+        delay_1ms(1);
     }
-    for (uint16_t i = 0; i < 256; i++)
-        printf("%2X", rx_buffer[i]);
+    uint8_t is_successful = 0;
+    if (ERROR == memory_compare(tx_buffer, rx_buffer, 256))
+    {
+        printf("\nErr:Data Read and Write aren't Matching.\n");
+        is_successful = 1;
+    }
+
+    /* spi qspi flash test passed */
+    if (0 == is_successful)
+    {
+        printf("\nSPI-GD25Q40 Test Passed!\n");
+    }
 
     while (1)
     {
@@ -148,4 +121,16 @@ int main(void)
         // ReceiveViaUART(uTextReceive, 9);
         // printf("Gpt %s\n", uTextReceive);
     }
+}
+
+ErrStatus memory_compare(uint8_t *src, uint8_t *dst, uint16_t length)
+{
+    while (length--)
+    {
+        if (*src++ != *dst++)
+        {
+            return ERROR;
+        }
+    }
+    return SUCCESS;
 }
